57f22e4128efa3f39db8587e777d9e796bb933d7
franciscotov <frank.ronaldo_17@hotmail.com>
2022-06-23 13:11:04 -0300

    decoradores

--- src/app/dtos/category.dto.ts
+++ src/app/dtos/category.dto.ts
@@ -1,11 +1,37 @@
-import { Category } from "../models/category.model";
-import { Product } from "../models/product.model";
+import { IsEnum, IsNotEmpty, IsOptional, IsUrl, Length, validateOrReject } from "class-validator";
+import { AccessType, Category } from "../models/category.model";
 
+export interface ICreateCategoryDto extends Omit<Category, 'id'| 'category'>{
+  // categoryId: Category['id'];
+}
 
-export interface CreateProductDto extends Omit<Product, 'id'| 'category'>{
-  categoryId: Category['id'];
+export class CreateCategoryDto implements ICreateCategoryDto{
+
+  @Length(5, 10)
+  @IsNotEmpty()
+  name!: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  image!: string;
+
+  @IsOptional()
+  @IsEnum(AccessType)
+  access?: AccessType | undefined;
 }
 
 
-export interface UpdateProductDto extends Partial<CreateProductDto>{
-}
+// export interface UpdateProductDto extends Partial<ICreateCategoryDto>{
+// }
+
+(async ()=> {
+  try {
+    const dto = new CreateCategoryDto();
+    dto.name = 'a1234';
+    dto.image = 'https://api.escuelajs.co/api/v1/products'
+    await validateOrReject(dto);
+
+  } catch (error) {
+    console.log(error)
+  }
+})();
--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,5 +1,11 @@
+export enum AccessType {
+  PRIVATE = 'PRIVATE',
+  PUBLIC = 'PUBLIC'
+}
+
 export interface Category {
   id:    number;
   name:  string;
   image: string;
+  access?: AccessType;
 }
